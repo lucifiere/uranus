42d19c568c71bb5f718cbe32920032e9868d3aa4
XD.Wang <lucifiere@126.com>
2016-09-02 17:55:32 +0800

    添加Spring Cache配置，添加数据库配置

---
+++ src/main/java/com/microweb/pojo/Demo.java
@@ -0,0 +1,29 @@
+package com.microweb.pojo;
+
+/**
+ *  Created by Created by XD.Wang on 2016/9/2.
+ */
+
+public class Demo {
+
+    private Long id;
+
+    private String des;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+}
