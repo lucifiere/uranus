bdbbd264f85b08e591a24616b4db1381566a2530
XD.Wang <lucifiere@126.com>
2016-10-18 18:43:07 +0800

    欢迎页

--- src/main/java/com/mvc/j/controller/WelcomPageController.java
+++ src/main/java/com/mvc/j/controller/WelcomPageController.java
@@ -1,7 +1,31 @@
 package com.mvc.j.controller;
 
+import com.mvc.j.service.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+
 /**
- * Created by Created by XD.Wang on 2016/10/18.
+ *  Created by Created by XD.Wang on 2016/10/18.
+ *  欢迎页
  */
+@Controller
 public class WelcomPageController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView test(){
+        ModelAndView mv = new ModelAndView("../html/index");
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("name", "王贤兑");
+        map.put("remark", "启动成功");
+        mv.addObject(map);
+        return mv;
+    }
+
 }
